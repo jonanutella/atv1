e94bb1394ba239e36ed6eaf6156c11adb33ce3a2
Moniquemayumi <163213589+Moniquemayumi@users.noreply.github.com>
2025-03-18 14:37:46 -0300

    Add files via upload

---
+++ appp.py
@@ -0,0 +1,6 @@
+paqueradela = "Davi Brito"
+Idade= "20"
+Condição= "Estudante"
+Estado_civil= "Casado"
+mulherdele = "AGATHA NUNES"
+print(f"A cantora Taylor swifit, conheceu {paqueradela} na sua primeira interação com {paqueradela}, logo pergunto sua idade e {paqueradela} respondeu: tenho {Idade} anos, sou {Condição} e {Estado_civil}, Taylor ficou decepcionada, mas continuou insistindo até que {mulherdele}, que é a 01 a bb linda de davi, INTERVIU E DEU UM SOCO NA RAPARIGA (Taylor Swift).")
