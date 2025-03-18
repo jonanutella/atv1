7d31d2847a59d5eb7943299418213ffc51b2bae1
jonanutella <158612346+jonanutella@users.noreply.github.com>
2025-03-18 14:43:45 -0300

    Update appp.py

--- appp.py
+++ appp.py
@@ -3,4 +3,5 @@ Idade= "20"
 Condição= "Estudante"
 Estado_civil= "Casado"
 mulherdele = "AGATHA NUNES"
-print(f"A cantora Taylor swifit, conheceu {paqueradela} na sua primeira interação com {paqueradela}, logo pergunto sua idade e {paqueradela} respondeu: tenho {Idade} anos, sou {Condição} e {Estado_civil}, Taylor ficou decepcionada, mas continuou insistindo até que {mulherdele}, que é a 01 a bb linda de davi, INTERVIU E DEU UM SOCO NA RAPARIGA (Taylor Swift).")
+presidente = "trump"
+print(f"A cantora Taylor swifit, conheceu {paqueradela} na sua primeira interação com {paqueradela}, logo pergunto sua idade e {paqueradela} respondeu: tenho {Idade} anos, sou {Condição} e {Estado_civil}, Taylor ficou decepcionada, mas continuou insistindo até que {mulherdele}, que é a 01 a bb linda de davi, INTERVIU E DEU UM SOCO NA RAPARIGA (Taylor Swift). Dai o {presidente} chegou e deu uma pisa em tudinho.")
